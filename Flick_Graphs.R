44ecf9769b44b4fa7bfe75cfa53b0a0915bb71da
Nihal <nsnihalshah@gmail.com>
2016-05-08 15:31:46 -0500

    Changed dependencies

--- Flick_Graphs.R
+++ Flick_Graphs.R
@@ -1,4 +1,6 @@
-data = read.csv("data/Snap_agg.csv",header=FALSE)
+library(scatterplot3d)
+library(rgl)
+data = read.csv("data/Snap Left Hand North.csv",header=FALSE)
 gyro_data = data.frame(data$V4, data$V5,data$V6)
 acc_data = data.frame(data$V1, data$V2,data$V3)
 scatterplot3d(acc_data$data.V1, acc_data$data.V2, acc_data$data.V3, xlab = "x", ylab="y", zlab="z",pch=16, highlight.3d = TRUE)
